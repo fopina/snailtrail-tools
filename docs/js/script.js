4bfe126cfc47c2b159efafbc51b0738527867693
Filipe Pina <63779195+fopinappb@users.noreply.github.com>
2022-07-01 13:34:12 +0100

    docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -329,192 +329,38 @@ document.addEventListener('DOMContentLoaded', function () {
     var ctx = document.getElementById('myChart');
 
     if (ctx) {
-      var myCanvas = ctx.getContext('2d');
-      var myChart = new Chart(myCanvas, {
-        type: 'line',
-        data: {
-          labels: ['Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-          datasets: [{
-            label: 'Last 6 months',
-            data: [35, 27, 40, 15, 30, 25, 45],
-            cubicInterpolationMode: 'monotone',
-            tension: 0.4,
-            backgroundColor: ['rgba(95, 46, 234, 1)'],
-            borderColor: ['rgba(95, 46, 234, 1)'],
-            borderWidth: 2
-          }, {
-            label: 'Previous',
-            data: [20, 36, 16, 45, 29, 32, 10],
-            cubicInterpolationMode: 'monotone',
-            tension: 0.4,
-            backgroundColor: ['rgba(75, 222, 151, 1)'],
-            borderColor: ['rgba(75, 222, 151, 1)'],
-            borderWidth: 2
-          }]
+      fetch("https://raw.githubusercontent.com/fopina/snailtrail-tools/data/log.txt")
+      .then(response => response.text())
+      .then(data => data.trim().split("\n").map(x => x.split(' ')))
+      .then(data => data.map(x => {
+        return {
+          x: `20${x[0]}`.replace('T', ' '),
+          y: parseInt(x[1]) / 1000
+        }
+      }))
+      .then(points => {
+        var myCanvas = ctx.getContext('2d');
+        var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            datasets: [{
+                label: 'Coefficent',
+                data: points
+            }],
         },
-        options: {
-          scales: {
-            y: {
-              min: 0,
-              max: 100,
-              ticks: {
-                stepSize: 25
-              },
-              grid: {
-                display: false
+          options: {
+              scales: {
+                  x: {
+                      type: 'time',
+                      time: {
+                        unit: 'day'
+                    }
+                  }
               }
-            },
-            x: {
-              grid: {
-                color: gridLine
-              }
-            }
-          },
-          elements: {
-            point: {
-              radius: 2
-            }
-          },
-          plugins: {
-            legend: {
-              position: 'top',
-              align: 'end',
-              labels: {
-                boxWidth: 8,
-                boxHeight: 8,
-                usePointStyle: true,
-                font: {
-                  size: 12,
-                  weight: '500'
-                }
-              }
-            },
-            title: {
-              display: true,
-              text: ['Visitor statistics', 'Nov - July'],
-              align: 'start',
-              color: '#171717',
-              font: {
-                size: 16,
-                family: 'Inter',
-                weight: '600',
-                lineHeight: 1.4
-              }
-            }
-          },
-          tooltips: {
-            mode: 'index',
-            intersect: false
-          },
-          hover: {
-            mode: 'nearest',
-            intersect: true
           }
-        }
       });
-      charts.visitors = myChart;
-    }
-    /* Customers chart */
-
-
-    var customersChart = document.getElementById('customersChart');
-
-    if (customersChart) {
-      var customersChartCanvas = customersChart.getContext('2d');
-      var myCustomersChart = new Chart(customersChartCanvas, {
-        type: 'line',
-        data: {
-          labels: ['Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-          datasets: [{
-            label: '+958',
-            data: [90, 10, 80, 20, 70, 30, 50],
-            tension: 0.4,
-            backgroundColor: function backgroundColor(context) {
-              var chart = context.chart;
-              var ctx = chart.ctx,
-                  chartArea = chart.chartArea;
-
-              if (!chartArea) {
-                // This case happens on initial chart load
-                return null;
-              }
-
-              return getGradient(ctx, chartArea);
-            },
-            borderColor: ['#fff'],
-            borderWidth: 2,
-            fill: true
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              display: false
-            },
-            x: {
-              display: false
-            }
-          },
-          elements: {
-            point: {
-              radius: 1
-            }
-          },
-          plugins: {
-            legend: {
-              position: 'top',
-              align: 'end',
-              labels: {
-                color: '#fff',
-                size: 18,
-                fontStyle: 800,
-                boxWidth: 0
-              }
-            },
-            title: {
-              display: true,
-              text: ['New Customers', '28 Daily Avg.'],
-              align: 'start',
-              color: '#fff',
-              font: {
-                size: 16,
-                family: 'Inter',
-                weight: '600',
-                lineHeight: 1.4
-              },
-              padding: {
-                top: 20
-              }
-            }
-          },
-          maintainAspectRatio: false
-        }
-      });
-      customersChart.customers = myCustomersChart;
+     })
     }
   })();
-  /* Change data of all charts */
 
-
-  function addData() {
-    var darkMode = localStorage.getItem('darkMode');
-
-    if (darkMode === 'enabled') {
-      gridLine = '#37374F';
-      titleColor = '#EFF0F6';
-    } else {
-      gridLine = '#EEEEEE';
-      titleColor = '#171717';
-    }
-
-    if (charts.hasOwnProperty('visitors')) {
-      charts.visitors.options.scales.x.grid.color = gridLine;
-      charts.visitors.options.plugins.title.color = titleColor;
-      charts.visitors.options.scales.y.ticks.color = titleColor;
-      charts.visitors.options.scales.x.ticks.color = titleColor;
-      charts.visitors.update();
-    }
-  }
-
-  addData();
 });
