1fd9824746b770264f36fabafa2b20e548e1143a
Filipe Pina <fopina@gmail.com>
2023-04-13 12:01:19 +0100

    linting

--- snail/gql.py
+++ snail/gql.py
@@ -50,17 +50,16 @@ query microwave_promise($params: MicrowaveParams) {
 }
 '''
 
+
 class Client(requests.Session):
     def __init__(self, url):
         super().__init__()
         self.url = url
-    
+
     def query(self, operation, variables, query, auth=None):
         kw = {}
         if auth:
-            kw['headers'] = {
-                'Auth': auth
-            }
+            kw['headers'] = {'Auth': auth}
         r = self.post(
             self.url,
             json={
@@ -68,7 +67,7 @@ class Client(requests.Session):
                 'variables': variables,
                 'query': query,
             },
-            **kw
+            **kw,
         )
         r.raise_for_status()
         r = r.json()
@@ -82,28 +81,20 @@ class Client(requests.Session):
 
     def marketplace_count(self):
         x = self.query('getAllSnail', {}, GQL_MARKETPLACE)
-        return {
-            k: v['count']
-            for k, v in x.items()
-        }
-    
+        return {k: v['count'] for k, v in x.items()}
+
     def burn_coef(self, token_id: int, signature: str, address: str, auth: str):
         return self.query(
             'microwave_promise',
-            {
-                'params': {
-                    'token_ids':[token_id],
-                    'signature': signature,
-                    'address': address,
-                    'use_scroll': False
-                }
-            },
+            {'params': {'token_ids': [token_id], 'signature': signature, 'address': address, 'use_scroll': False}},
             GQL_BURN,
             auth=auth,
         )['microwave_promise']['payload']['coef']
 
 
 def proxied_client():
-    c_id = subprocess.check_output(['docker', 'run', '--rm', '-dp', '9999:8888', 'fopina/random:snailtrail-gotls'], text=True)
+    c_id = subprocess.check_output(
+        ['docker', 'run', '--rm', '-dp', '9999:8888', 'fopina/random:snailtrail-gotls'], text=True
+    )
     atexit.register(lambda: subprocess.check_output(['docker', 'kill', c_id.strip()]))
     return Client('http://127.0.0.1:9999/graphql')
