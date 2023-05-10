cd3004c61e4c36f2872d9c2cbbf3adb5966f941c
Filipe Pina <fopina@gmail.com>
2023-05-10 12:25:20 +0100

    x

--- main_pop_dune.py
+++ main_pop_dune.py
@@ -39,6 +39,7 @@ def main(argv=None):
             "query": "query GetResult($query_id: Int!, $parameters: [Parameter!]!) {\n  get_result_v3(query_id: $query_id, parameters: $parameters) {\n    job_id\n    result_id\n    error_id\n    __typename\n  }\n}\n",
         },
     )
+    r.raise_for_status()
     execution_id = r.json()['data']['get_result_v3']['result_id']
     r = requests.post(
         'https://app-api.dune.com/v1/graphql',
@@ -48,6 +49,7 @@ def main(argv=None):
             "query": "query GetExecution($execution_id: String!, $query_id: Int!, $parameters: [Parameter!]!) {\n  get_execution(\n    execution_id: $execution_id\n    query_id: $query_id\n    parameters: $parameters\n  ) {\n    execution_queued {\n      execution_id\n      execution_user_id\n      position\n      execution_type\n      created_at\n      __typename\n    }\n    execution_running {\n      execution_id\n      execution_user_id\n      execution_type\n      started_at\n      created_at\n      __typename\n    }\n    execution_succeeded {\n      execution_id\n      runtime_seconds\n      generated_at\n      columns\n      data\n      __typename\n    }\n    execution_failed {\n      execution_id\n      type\n      message\n      metadata {\n        line\n        column\n        hint\n        __typename\n      }\n      runtime_seconds\n      generated_at\n      __typename\n    }\n    __typename\n  }\n}\n",
         },
     )
+    r.raise_for_status()
     data = r.json()['data']
     holders = data['get_execution']['execution_succeeded']['data']
     timestamp = data['get_execution']['execution_succeeded']['generated_at']
