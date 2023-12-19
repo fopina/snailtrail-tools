c2cacf2b8b76593ef0b71ab97a75b2547ebc33c9
Filipe Pina <hzlu1ot0@duck.com>
2023-12-19 01:14:00 +0000

    median tx fee

---
+++ main_tx_fee.py
@@ -0,0 +1,35 @@
+import argparse
+from pathlib import Path
+from datetime import datetime
+import struct
+from snail import Client
+
+
+def parser():
+    p = argparse.ArgumentParser(prog=__name__)
+    p.add_argument('avax_rpc_url')
+    p.add_argument('-o', '--output', type=Path, help='output file to log timestamp and value')
+    p.add_argument('--binary-log', action='store_true', help='Use binary format for log')
+    return p
+
+
+def main(argv=None):
+    args = parser().parse_args(argv)
+    web3 = Client('', args.avax_rpc_url)
+    c = web3.web3.eth.gas_price
+    print(f'Median tx fee (nAVAX): {c / 10**9}')
+    if args.output:
+        now = datetime.utcnow()
+        print(now)
+        if args.binary_log:
+            with args.output.open('ab') as f:
+                f.write(struct.pack('>I', int(now.timestamp())))
+                # assume coefficent always under 65% (<=65535) to save some bytes!
+                f.write(struct.pack('>H', c))
+        else:
+            with args.output.open('a') as f:
+                f.write(f'{now:%y-%m-%dT%H:%M:%S} {c}\n')
+
+
+if __name__ == '__main__':
+    main()
